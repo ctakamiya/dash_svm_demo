9fd70387390c36492c4e888d24b957bc8035fff7
Workshop Admin <admin@cloudera.com>
2019-10-16 16:09:33 +0000

    Cleaned up launch script

--- 00_launch_app.py
+++ 00_launch_app.py
@@ -3,15 +3,19 @@
 !pip3 install -r requirements.txt
 
 # # Get engine ID
-# ## ...and construct app URL
-# #### Get requiered env variables 
+# ## ...and construct app URL from env variables 
 import os 
 engine_id = os.environ.get('CDSW_ENGINE_ID')
 cdsw_domain = os.environ.get('CDSW_DOMAIN')
 
-# #### Create app URL
+# # Print out URL
+# ### Raw text 
+"http://{}.{}".format(engine_id,cdsw_domain,engine_id,cdsw_domain)
+
+# ### clickable URL
 from IPython.core.display import HTML
-HTML('<a href="https://{}.{}">https://{}.{}</a>'.format(engine_id,cdsw_domain,engine_id,cdsw_domain))
+HTML('<a href="http://{}.{}">http://{}.{}</a>'
+     .format(engine_id,cdsw_domain,engine_id,cdsw_domain))
 
 # # Launch dash app
 !python3 app.py
